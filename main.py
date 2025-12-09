1ad8469305e4945714655998898a1e2bb3e3b50f
bubblegum47 <97658660+bubblegum47@users.noreply.github.com>
2025-12-09 13:59:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ first = int(input("처음 숫자를 입력하세요: "))
 second = int(input("두 번째 숫자를 입력하세요 : "))
 
 sum = first + second
+diff = first - second
 
-print("두 숫자의 합은", sum, "입니다")
-
-
+print("두 숫자의 차이는", diff, "입니다.")
