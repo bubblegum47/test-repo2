0cdf92d17b6ad86025fe6aabe883f692a46883ed
yegang <yegangmun3@gmail.com>
2025-12-09 14:11:04 +0900

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-first = int(input("처음 숫자를 입력하세요: "))
-second = int(input("두 번째 숫자를 입력하세요 : "))
+first = input("처음 숫자를 입력하세요: ")
+second =input("두 번째 숫자를 입력하세요 : ")
 
-sum = first + second
-diff = first - second
+num1 = int(first)
+num2 = int(second)
 
-print("두 숫자의 차이는", diff, "입니다.")
+result = num1 + num2
+
+
+print("result:", result)
