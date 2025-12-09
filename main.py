a6f0a3ca94dea8e77a9985e1bcb33064a3cebf91
yegang <yegangmun3@gmail.com>
2025-12-09 14:14:55 +0900

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ second =input("두 번째 숫자를 입력하세요 : ")
 num1 = int(first)
 num2 = int(second)
 
-sum = num1 + num2
-sub = num1 - num2
+sum_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
 
-print("result:", sub)
+print("result:", sub_result)
+print("result:", sum_result)
+print("result:", mul_result)
